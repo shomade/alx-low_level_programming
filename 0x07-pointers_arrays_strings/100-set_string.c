ef7d0333ca876ecda8349f087afeccb68d488afd
shomade titilayo <shomadetitilayo6667@gmail.com>
2022-09-26 14:54:13 -0700

    tasks

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,14 +1,24 @@
-#include "holberton.h"
+
+#include <stdio.h>
 
 /**
-  * set_string - Sets the value of a pointer to char
-  * @s: the value to modify
-  * @to: the value to assign
+  * print_diagsums - Print the sum of two diagonals of square matrix
+  *
+  * @a: the matrix
+  * @size: the size
   *
   * Return: Nothing.
   */
-void set_string(char **s, char *to)
+void print_diagsums(int *a, int size)
 {
-	*s = to;
+	int b, sum1 = 0, sum2 = 0;
+
+	for (b = 0; b < size; b++)
+	{
+		sum1 += a[(size + 1) * b];
+		sum2 += a[(size - 1) * (b + 1)];
+	}
+
+	printf("%d, %d\n", sum1, sum2);
 }
 
